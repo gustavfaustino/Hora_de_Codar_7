30abad56e1351f2b871f720f4c7f8a12cc835bf0
gustavfaustino <gustav.faustino107@gmail.com>
2024-11-12 00:07:30 -0300

    Exercício 15, final

--- src/HeightPlay.java
+++ src/HeightPlay.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class HeightPlay {
+public class HeightPlay {
+    public static void main(String[] args) {
+        /*Anacleto tem 1, 50 metro e cresce 2 centímetros por ano, enquanto Felisberto tem 1, 1 O metro e cresce 3
+        centímetros por ano.Construa um algoritmo que calcule e imprima quantos anos serão necessários para
+        que Felisberto seja maior que Anacleto.*/
+
+        int anos = 0;
+        double alturaAnacleto = 1.50, alturaFelisberto = 1.10;
+        while (alturaAnacleto > alturaFelisberto) {
+            anos += 1;
+            alturaAnacleto += 0.02;
+            alturaFelisberto += 0.03;
+        }
+        System.out.println("Anacleto ficara maior que Felisberto em " + anos + " anos.");
+    }
 }
